c6cee2a51dbb991f4c05429456766b1fe199ad13
Francisco Seijas <panchoseijas@gmail.com>
2024-10-16 23:36:59 -0300

    Fixed theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,22 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useColorScheme } from "nativewind";
+import { Appearance } from "react-native";
+import { useEffect } from "react";
 
 const queryClient = new QueryClient({});
 
 export default function RootLayout() {
-  const { colorScheme } = useColorScheme();
-  console.log(colorScheme);
+  const { colorScheme, setColorScheme, toggleColorScheme } = useColorScheme();
+
+  useEffect(() => {
+    const subscription = Appearance.addChangeListener(({ colorScheme }) => {
+      toggleColorScheme();
+    });
+
+    return () => subscription.remove();
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
